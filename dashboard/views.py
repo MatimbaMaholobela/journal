930ac518d05bbc535d51b9f8ac57ecc0c70ba842
unknown <matimbascarra101@gmail.com>
2024-03-14 19:58:12 +0200

    made the page to be little bit nicer

--- dashboard/views.py
+++ dashboard/views.py
@@ -4,5 +4,5 @@ from django.shortcuts import render
 def home(request):
     return render(request, "home.html")
 
-def journal(request):
+def journal_entry(request):
     return render(request,"journal.html")
