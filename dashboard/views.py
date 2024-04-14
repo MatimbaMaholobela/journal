81d122e8dfb88fd1a5cb64ad94d9a6eac8a3f58c
MagobiyaneBanele <163750118+MagobiyaneBanele@users.noreply.github.com>
2024-04-14 18:38:15 +0200

    Update views.py

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import redirect, render
+# import the journal from models and pass variables
+
 
 # Create your views here.
 def home(request):
@@ -8,4 +11,24 @@ def journal_entry(request):
     return render(request,"journal.html")
     
 def login(request):
-    return render(request,"login.html")
+    return render(request,"login.html") 
+
+def login_cred(request):
+    if request.method == "POST":
+        username=request.POST.get("username")
+        password= request.POST.get("password")
+        print("Username: ", username)
+        print("Password: ", password)
+        
+        return render(request,"home.html") 
+    
+    else:
+        return HttpResponse(status=404)
+    
+def journal_login(request):
+    if request.method == "POST":
+        entryDate=request.POST.get("entryDate")
+        entryTitle=request.POST.get("entryTitle")
+        entryContent=request.POST.get("entryContent")
+
+        
