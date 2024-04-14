6bb8ae29b940c8322b5601c79607439e11ea9003
MagobiyaneBanele <163750118+MagobiyaneBanele@users.noreply.github.com>
2024-04-14 18:37:21 +0200

    Update models.py
    Updated models

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class JournalEntry(models.Model):
+    date=models.DateTimeField(blank=False)
+    title=models.TextField(blank=False,max_length = 100)
+    journal_entry= models.TextField(blank=True,max_length = 120)
+    
