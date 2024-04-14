cdb7f6e2e2a257cfde7efaf9c467ad244b765af7
MagobiyaneBanele <163750118+MagobiyaneBanele@users.noreply.github.com>
2024-04-14 18:36:27 +0200

    Update admin.py

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from .models import JournalEntry
+
 # Register your models here.
+class JournalEntryAdmin(admin.ModelAdmin):
+    list_display = ('date','title','journal_entry')
+ 
+ 
+ 
+admin.site.register(JournalEntry, JournalEntryAdmin)
